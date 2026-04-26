054302c93b7721330c97870793cb6782158b13d1
Alisha Asmat <alisha.itseries@gmail.com>
2026-04-26 12:24:37 +0500

    added JS practice projects

---
+++ Tic Tac Toe/app.js
@@ -0,0 +1,78 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newBtn = document.querySelector("#new-btn");
+let msg = document.querySelector("#msg");
+let msgContainer = document.querySelector(".msg-container");
+
+let turn0 = true;
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+// reset button
+const resetGame = () => {
+  turn0 = true;
+  enableBtn();
+  msgContainer.classList.add("hide");
+};
+
+// disable buttons
+const disableBtn = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+// enable buttons
+const enableBtn = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+// show winner
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations!, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBtn();
+};
+
+// winner Check
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+    if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        console.log("winner", pos1Val);
+        showWinner(pos1Val);
+      }
+    }
+  }
+};
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    console.log("button was clicked!");
+    if (turn0) {
+      box.innerText = "O";
+      turn0 = false;
+    } else {
+      box.innerText = "X";
+      turn0 = true;
+    }
+    box.disabled = true;
+    checkWinner();
+  });
+});
+
+newBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
